508b0670c741546365e314aa3025ed48e9ce7491
Al Tanner <8190834+altanner@users.noreply.github.com>
2019-11-05 15:42:11 +0000

    Update geo_harvester.py
    Former-commit-id: 8877d33fd8d6390b34b7c48da8ceaad06430b582

--- mood_observatory/bristol_tweets/geo_harvester.py
+++ mood_observatory/bristol_tweets/geo_harvester.py
@@ -2,14 +2,18 @@ import tweepy
 import json
 import sys
 import signal
+import subprocess
+import os
 from pymongo import MongoClient
 
 import mongo_ops
 import credentials
 import geo_boxes
+import env_config
 
 def signal_handler(sig, frame):
     print(f"\n\nCtrl-c, ok got it, just a second while I try to exit gracefully...")
+    mongo_ops.stop_mongo()
     sys.exit(0)
 
 class StreamListener(tweepy.StreamListener):
@@ -47,12 +51,42 @@ class StreamListener(tweepy.StreamListener):
         print("New tweet.")
         db.geotweets_collection.insert_one(datajson)
 
+        # export the most recent tweet as csv so it can be sentiment analysed
+        mongo_ops.export_latest_tweet(mongoexport_executable_path)
+
 
 if __name__ == "__main__":
 
+    ## Set up interrupt signal handling and environment paths
     signal.signal(signal.SIGINT, signal_handler)
-    mongo_ops.check_mongo()
+    env = env_config.EnvironmentConfig()
 
+    ## check if MongoDB is present and correct
+    try:
+        mongod_executable_path = subprocess.check_output(['which', 'mongod']).decode('utf-8').strip()
+    except:
+        print(f"You don't seem to have MongoDB installed. Stopping.")
+        sys.exit()
+    try:
+        mongoexport_executable_path = subprocess.check_output(['which', 'mongoexport']).decode('utf-8').strip()
+    except:
+        print(f"Mongoexport seems missing... stopping.")
+        sys.exit()
+    try:
+        mongodump_executable_path = subprocess.check_output(['which', 'mongodump']).decode('utf-8').strip()
+    except:
+        print(f"Mongodump seems missing... stopping.")
+        sys.exit()
+
+    ## Check or make directory structure
+    if not os.path.exists(env.run_folder + '/db'):
+        print(f"MongoDB database folder seems absent, creating folder...")
+        os.makedirs(env.run_folder + '/db')
+    if not os.path.exists(env.run_folder + '/db_logs'):
+        print(f"DB log folder seems absent, creating folder...")
+        os.makedirs(env.run_folder + '/db_logs')
+
+    mongo_ops.start_mongo(mongod_executable_path, env.db_path, env.db_log_filename)
     auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
     auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
     stream_listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
