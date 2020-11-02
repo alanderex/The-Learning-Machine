832866bf70262869cc6edfe753a3c4c7cc8ce2bf
leriomaggio <valerio.maggio@gmail.com>
2020-11-02 15:56:02 +0000

    New methods to (de-)serialise items sampled and blacklisted; new method to discard samples (put in black list)
    Now lists of sampled items and discarded ones are handled via sets, and serialised with an hook on server shutdown.
    A TODO is still opened for serialisation of current model weights (per-session model)
    Currently, only blacklisted images are serialised and restored.

--- learning_machine/backend/datasets/sources.py
+++ learning_machine/backend/datasets/sources.py
@@ -5,11 +5,12 @@ Specify Data sources used to proxy access to available Torch Datasets
 from dataclasses import dataclass
 from torch.utils.data import Dataset, ConcatDataset
 from datasets.fer import FER
-from typing import Callable, Sequence, Union
+from typing import Callable, Sequence, Union, Set
 from PIL.Image import Image as PILImage
 from random import sample
 from hashlib import sha256
 from os import path
+from pathlib import Path
 
 SECRET_SPICE = "supersecrectspiceonthebackend"
 
@@ -64,6 +65,10 @@ def only_fer_training() -> Dataset:
 
 
 class DataSource:
+
+    BLACKLIST_SAMPLES = Path("indices_blacklist.txt")
+    RETURNED_SAMPLES = Path("indices_sampled.txt")
+
     def __init__(
         self,
         dataset_load_fn: DatasetLoadFn = default_load_fer_dataset,
@@ -71,8 +76,33 @@ class DataSource:
     ) -> None:
         self._dataset = None  # Instantiated once via property
         self._ds_load_fn = dataset_load_fn
-        self._items_sampled = set()  # implementing memory-map
+        self._items_sampled = self._init_list(
+            self.RETURNED_SAMPLES
+        )  # implementing memory-map
+        self._blacklist = self._init_list(
+            self.BLACKLIST_SAMPLES
+        )  # Set of indices to exclude *ever*
         self._emotions = target_emotions
+
+    def _init_list(self, samples_list_filepath: Path) -> Set:
+        if samples_list_filepath.exists():
+            return self._load_from(samples_list_filepath)
+        return set()
+
+    @staticmethod
+    def _load_from(samples_list_filepath: Path) -> Set:
+        with open(samples_list_filepath) as f:
+            indices = f.read().strip().split(",")
+            indices = filter(lambda idx: len(idx.strip()) > 0, indices)
+            indices = map(int, indices)
+        indices = set(indices)
+        return indices
+
+    @staticmethod
+    def _serialise(indices: Set, target_list_filepath: Path) -> None:
+        with open(target_list_filepath, "w") as f:
+            line = ",".join(map(str, indices))
+            f.write(f"{line}\n")
 
     @property
     def dataset(self) -> Dataset:
@@ -101,7 +131,8 @@ class DataSource:
 
     def get_random_samples(self, k: int) -> Sequence[Sample]:
         samples = list()
-        pool = set(range(len(self.dataset))).difference(self._items_sampled)
+        excluded = self._items_sampled.union(self._blacklist)
+        pool = set(range(len(self.dataset))).difference(excluded)
         rnd_indices = sample(pool, k=k)
         for sample_idx in rnd_indices:
             samples.append(self[sample_idx])
@@ -111,6 +142,21 @@ class DataSource:
         #     self["c69b31e495d132603ae3c8e72dc236ed0e1889bd7b62b0e2f431161ca5ad0c1f_2f6"]
         # )
         return samples
+
+    def discard_sample(self, index: Union[str, int]):
+        try:
+            index = int(index)
+        except ValueError:
+            index = Sample.retrieve_index(str(index))
+        self._blacklist.add(index)
+
+    def serialise_session(self) -> None:
+        # Serialise Items Sampled
+        # self._serialise(self._items_sampled, self.RETURNED_SAMPLES)
+        # Serialise the MODEL WEIGHTS
+        # TODO
+        # Serialise Blacklist
+        self._serialise(self._blacklist, self.BLACKLIST_SAMPLES)
 
 
 def load_fer_dataset_lazy() -> DataSource:
