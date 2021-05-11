f049fc6a18998fb5e5787bd1d3897e523f0672c3
leriomaggio <valerio.maggio@gmail.com>
2021-05-12 00:35:52 +0100

    removed mood_observatory folder

--- mood_observatory/geo_harvester/modules/csv2json.py
+++
@@ -1,8 +0,0 @@
-import csv
-import json
-import sys
-
-# quick and dirty, here as a reminder of what I need to do! :)
-print(json.dumps(list(csv.reader(open(sys.argv[1])))))
-
-# needs making pretty and file management
